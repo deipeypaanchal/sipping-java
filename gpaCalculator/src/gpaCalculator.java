fc3c6469c2487f93b4867847d11e16a04a6b8cf1
Deipey Paanchal <109501278+deipeypaanchal@users.noreply.github.com>
2022-07-18 19:59:19 +0530

    Add files via upload

---
+++ gpaCalculator/src/gpaCalculator.java
@@ -0,0 +1,15 @@
+public class gpaCalculator {
+    public static double[] calculateGPA(int[] studentIdList, char[][] studentsGrades) {
+        return new double[0];
+    }
+
+    public static int[] getStudentsByGPA(double lower, double higher, int[] studentIdList, char[][] studentsGrades) {
+        // perform parameter validation. Return null if passed parameters are not valid
+
+
+        // invoke calculateGPA
+
+        // construct the result array and return it. You would need an extra for loop to compute the size of the resulting array
+        return studentIdList;
+    }
+}
